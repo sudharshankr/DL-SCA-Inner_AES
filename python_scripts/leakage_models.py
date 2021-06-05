1a3fa3f799090d85029243c3d8e29c5010f5fb00
Sudharshan Kumar <sudharshankr94@gmail.com>
2021-06-05 10:10:48 +0200

    Added model for computing leakage in batches

--- python_scripts/leakage_models.py
+++ python_scripts/leakage_models.py
@@ -19,3 +19,19 @@ def calc_hypothesis_round_3(plaintext_byte, key_guesses):# key_byte, gamma=0, de
                             ^ key_guesses[:, 2]]]
 
     return hw
+
+
+def calc_hypothesis_round_3_batch(plaintexts, key_guesses, batch_size):
+    m1 = m2 = 2
+    # S(m1 * S(m2 * S(p0 ^ k0) xor delta) xor gamma)
+    hw = hamming_lookup[aes_sbox[
+        galois_mult_np(
+            aes_sbox[
+                galois_mult_np(
+                    aes_sbox[
+                        plaintexts.reshape(batch_size, 1) ^ key_guesses[:, 0]],
+                    m2)
+                ^ key_guesses[:, 1]], m1)
+        ^ key_guesses[:, 2]]]
+
+    return hw
